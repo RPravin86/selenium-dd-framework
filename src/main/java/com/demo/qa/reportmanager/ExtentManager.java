81f971782f796e860172face304217d3f49eaf91
rPravin86 <62685733+RPravin86@users.noreply.github.com>
2026-08-16 12:49:30 +0530

    fix: update ExtentManager for AppConfig class refactor

--- src/main/java/com/demo/qa/reportmanager/ExtentManager.java
+++ src/main/java/com/demo/qa/reportmanager/ExtentManager.java
@@ -9,25 +9,29 @@ import com.demo.qa.core.AppConfig;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class ExtentManager implements AppConfig {
+public class ExtentManager {
 
-	private static ExtentReports extentReport;
+    private static ExtentReports extentReport;
 
-	/**
-	 * This is a static method that creates and configures ExtentReports object.
-	 * This method sets the theme of the report to STANDARD and title to "Test Report".
-	 * 
-	 * @return ExtentReports
-	 */
-	public synchronized static ExtentReports getExtentReports() {
-		if (extentReport == null) {
-			String date = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(new Date());
-			ExtentSparkReporter htmlReporter = new ExtentSparkReporter(REPORT_PATH + date);
-			extentReport = new ExtentReports();
-			extentReport.attachReporter(htmlReporter);
-			htmlReporter.config(
-					ExtentSparkReporterConfig.builder().theme(Theme.STANDARD).documentTitle(REPORT_TITLE).build());
-		}
-		return extentReport;
-	}
+    /**
+     * This is a static method that creates and configures ExtentReports object.
+     * This method sets the theme of the report to STANDARD and title to "Test Report".
+     *
+     * @return ExtentReports
+     */
+    public synchronized static ExtentReports getExtentReports() {
+        if (extentReport == null) {
+            String date = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(new Date());
+            ExtentSparkReporter htmlReporter =
+                    new ExtentSparkReporter(AppConfig.REPORT_PATH + date);
+            extentReport = new ExtentReports();
+            extentReport.attachReporter(htmlReporter);
+            htmlReporter.config(
+                    ExtentSparkReporterConfig.builder()
+                            .theme(Theme.STANDARD)
+                            .documentTitle(AppConfig.REPORT_TITLE)
+                            .build());
+        }
+        return extentReport;
+    }
 }
