d8fa1a97a0e1b0c59fa89659337f302501d1ca25
rPravin86 <62685733+RPravin86@users.noreply.github.com>
2026-08-16 19:11:49 +0530

    refactor: encapsulate PerfumePage locators

--- src/test/java/com/demo/qa/pageobjects/PerfumePage.java
+++ src/test/java/com/demo/qa/pageobjects/PerfumePage.java
@@ -19,31 +19,36 @@ public class PerfumePage {
 
     private static final Duration FILTER_WAIT_TIMEOUT = Duration.ofSeconds(10);
 
-    private final WebDriver driver;
+    private static final String FACET_OPTION_LABEL =
+            "//div[@class='facet-option__label']//div[text() = '%s']/ancestor::a";
+    private static final String FACET_LABEL =
+            "//div[@class='facet']//div[text() = '%s']";
+    private static final String SELECTED_FACET_VALUE =
+            "//button[@class='selected-facets__value' and text() = '%s']";
 
-    public By productTitle = By.cssSelector("div[class='text top-brand']");
-    public String facetDDOptionLabel = "//div[@class='facet-option__label']//div[text() = '%s']/ancestor::a";
-    public String facetDDLabel = "//div[@class='facet']//div[text() = '%s']";
-    public String selectedFacetValue = "//button[@class='selected-facets__value' and text() = '%s']";
+    private final WebDriver driver;
+    private final By productTitle = By.cssSelector("div[class='text top-brand']");
+    private final WebDriverWait wait;
 
     public PerfumePage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, FILTER_WAIT_TIMEOUT);
     }
 
     public void applyFilter(Map<String, String> filterMap) {
-        for (String key : filterMap.keySet()) {
-            String value = filterMap.get(key);
-            WebElement element = driver.findElement(By.xpath(String.format(facetDDLabel, key)));
+        for (Map.Entry<String, String> entry : filterMap.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            WebElement element = driver.findElement(By.xpath(String.format(FACET_LABEL, key)));
             ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
             element.click();
-            element = driver.findElement(By.xpath(String.format(facetDDOptionLabel, value)));
+            element = driver.findElement(By.xpath(String.format(FACET_OPTION_LABEL, value)));
             ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
             element.click();
 
             // Wait for the selected filter to become visible instead of using a fixed thread sleep.
-            WebDriverWait wait = new WebDriverWait(driver, FILTER_WAIT_TIMEOUT);
             element = wait.until(ExpectedConditions.visibilityOfElementLocated(
-                    By.xpath(String.format(selectedFacetValue, value))));
+                    By.xpath(String.format(SELECTED_FACET_VALUE, value))));
 
             Assert.assertTrue(element.isDisplayed(), "Filter not applied: " + value);
             Report.log(Status.PASS, "Filter applied: [ " + key + ": " + value + " ]");
