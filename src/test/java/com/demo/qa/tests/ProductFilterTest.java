b640e680ee70770c09381b8c59712e2124acc963
rPravin86 <62685733+RPravin86@users.noreply.github.com>
2026-08-16 18:07:56 +0530

    refactor: simplify ProductFilterTest JSON data handling

--- src/test/java/com/demo/qa/tests/ProductFilterTest.java
+++ src/test/java/com/demo/qa/tests/ProductFilterTest.java
@@ -4,14 +4,11 @@ import com.demo.qa.core.AppConfig;
 import com.demo.qa.core.BaseTest;
 import com.demo.qa.core.ObjectRepo;
 import com.demo.qa.utilities.JsonFileReader;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Listeners(com.demo.qa.utilities.TestListener.class)
@@ -30,20 +27,12 @@ public class ProductFilterTest extends BaseTest {
 
     @DataProvider
     public Object[][] filterData() throws Exception {
-        JSONArray jsonArray = JsonFileReader.readJson(AppConfig.TEST_RESOURCE_PATH + "/filter.json"
-                , "Criteria");
-        Object[][] dataObj = new Object[jsonArray.size()][1];
+        List<Map<String, String>> filterData = JsonFileReader.readJson(
+                AppConfig.TEST_RESOURCE_PATH + "/filter.json",
+                "Criteria");
 
-        for (int i = 0; i < jsonArray.size(); i++) {
-            Map<String, String> map = new HashMap<>();
-            JSONObject jsonObject = (JSONObject) jsonArray.get(i);
-            for (Object key : jsonObject.keySet()) {
-                map.put((String) key, (String) jsonObject.get(key));
-            }
-            dataObj[i][0] = map;
-        }
-
-        System.out.println(Arrays.toString(dataObj));
-        return dataObj;
+        return filterData.stream()
+                .map(data -> new Object[]{data})
+                .toArray(Object[][]::new);
     }
 }
