dab500908f3de9695da66b210823c572893db29c
rPravin86 <62685733+RPravin86@users.noreply.github.com>
2026-09-05 14:54:37 +0530

    testdata: add authentication user record

---
+++ src/test/java/com/demo/qa/testdata/UserData.java
@@ -0,0 +1,22 @@
+package com.demo.qa.testdata;
+
+public record UserData(
+        String name,
+        String email,
+        String password,
+        String title,
+        String birthDay,
+        String birthMonth,
+        String birthYear,
+        String firstName,
+        String lastName,
+        String company,
+        String address,
+        String address2,
+        String country,
+        String state,
+        String city,
+        String zipCode,
+        String mobileNumber
+) {
+}
